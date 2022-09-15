4ebb848ec224165345ca00ff53facf41b6cf5b72
Karan Abraham <abrahamk2001@gmail.com>
2022-09-15 13:13:29 +0200

    Implemented RecyclerView

---
+++ app/src/main/java/com/example/alphabetbookapp/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.alphabetbookapp
+
+data class ItemsViewModel(val text: String) {
+}
--- app/src/main/java/com/example/alphabetbookapp/MainActivity.kt
+++ app/src/main/java/com/example/alphabetbookapp/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.alphabetbookapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createButtonGrid()
-    }
 
-    private fun createButtonGrid() {
-        for (letter in 'A'..'Z') {
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (c in 'A'..'Z') {
+            data.add(ItemsViewModel(c.toString()))
         }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
     }
 }
